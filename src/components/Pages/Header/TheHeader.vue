c957d267f099bf7ea6ef2f706bfe5090b9df8e0d
Kotoghyan <kotoghyan.karapet@gmail.com>
2023-04-07 03:49:37 +0400

    CREATE: Header: ADD: shared: button, inpput: logo

---
+++ src/components/Pages/Header/TheHeader.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="root">
+  <div class="container">
+    <img class="logo" v-bind:src="logo" alt="Market Logo">
+    <shared-input></shared-input>
+    <shared-button></shared-button>
+  </div>
+</div>
+</template>
+
+
+
+<script>
+import logo from '@/assets/logo.png'
+import SharedButton from "@/components/shared/SharedButton";
+import SharedInput from "@/components/shared/SharedInput";
+export default {
+  data() {
+    return {
+      logo,
+    }
+  },
+  components:{SharedInput, SharedButton},
+  name: "TheHeader"
+}
+</script>
+
+<style scoped>
+  .root{
+    height: 100px;
+  }
+  .container{
+    padding: 24px;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .logo{
+    width: fit-content;
+    height: 100%;
+  }
+</style>
---
+++ src/components/shared/SharedButton.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+<!--    <button v-bind:class="cancel?'root':'cancel root'">{{title}}</button>-->
+    <button v-bind:class="[buttonParams]">{{buttonName()}}</button>
+  </div>
+</template>
+
+<script>
+import {watchEffect} from "vue";
+
+export default {
+  data() {
+    return {
+      title: 'buy',
+      cancel: true
+    }
+  },
+  methods:{
+    buttonName() {
+      return this.cancel ? this.title = 'buy' : this.title = 'cancel'
+    }
+  },
+
+  computed:{
+    buttonParams() {
+      return ['root', {cancel:!this.cancel}]
+    },
+  },
+  //todo harc Sonain, chsht em haskacel vor setup@ da nkarchutyan hamara propserov vor poxancum es?
+  setup(){
+    watchEffect(function (){
+      console.log()
+    })
+  },
+
+  // watch: {
+  //   title(title, cancel){
+  //     console.log(title)
+  //     cancel ? title = 'buy' : title = 'cancel'
+  //   },
+  // },
+  name: "SharedButton"
+}
+</script>
+
+<style scoped>
+.root {
+  padding: 12px 32px;
+  font-size: 14px;
+  letter-spacing: 0.7px;
+  text-transform: uppercase;
+  border: none;
+  border-radius: 4px;
+  background: #ffd500;
+}
+
+.cancel {
+  background: #D8000066;
+  color: white;
+}
+</style>
